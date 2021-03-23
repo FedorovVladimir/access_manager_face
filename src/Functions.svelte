8f45ff1ec46a1b92860331ca85d744b46060a251
FedorovVladimir <vladimir.fodorow@gmail.com>
2021-03-23 19:51:33 +0700

    Добавлена страница работы с функциями

--- src/Functions.svelte
+++ src/Functions.svelte
@@ -1,20 +1,95 @@
 <script>
-    import {getFetch} from "./main";
+    import {deleteFetch, getFetch, postFetch} from "./main";
 
     let functions = []
 
-    async function loadFunctions() {
+    let code = ""
 
+    let name = ""
+
+    async function loadFunctions() {
         functions = await getFetch("http://localhost:8080/functions")
         console.log(functions)
+    }
+
+    async function addFunction() {
+        let role = {
+            "code": code,
+            "name": name
+        }
+        await postFetch("http://localhost:8080/functions", role)
+        await loadFunctions()
+        code = ""
+        name = ""
+    }
+
+    async function deleteFunction(code) {
+        await deleteFetch("http://localhost:8080/functions/" + code)
+        await loadFunctions()
     }
 
     loadFunctions()
 </script>
 
 <main>
-    <p>Функции</p>
-    {#each functions as f}
-        <p>{f.code} {f.name}</p>
-    {/each}
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-2">
+            </div>
+            <div class="col-8">
+
+                <h1>Функции</h1>
+
+                <h3 class="mt-5 mb-3">Таблица функций</h3>
+
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">№</th>
+                        <th scope="col">Код</th>
+                        <th scope="col">Название</th>
+                        <th scope="col"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {#each functions as f, i}
+                        <tr>
+                            <th scope="row">{i + 1}</th>
+                            <td>{f.code}</td>
+                            <td>{f.name}</td>
+                            <td style="text-align: center">
+                                <span class="text-danger" style="cursor: pointer"
+                                      on:click="{() => deleteFunction(f.code)}">
+                                    <svg class="bi bi-trash" fill="currentColor" height="20" viewBox="0 0 20 20"
+                                         width="20" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                                              fill-rule="evenodd"/>
+                                    </svg>
+                                </span>
+                            </td>
+                        </tr>
+                    {/each}
+                    </tbody>
+                </table>
+
+                <h3 class="mt-5 mb-3">Добавить функцию</h3>
+
+                <form>
+                    <div class="mb-3">
+                        <label class="form-label" for="code">Код <span class="text-danger">*</span></label>
+                        <input bind:value={code} class="form-control" id="code" required type="text">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="name">Название <span class="text-danger">*</span></label>
+                        <input bind:value={name} class="form-control" id="name" required type="text">
+                    </div>
+                </form>
+                <button class="btn btn-secondary mb-5" id="send" on:click={addFunction}>Создать функцию</button>
+
+            </div>
+            <div class="col-2">
+            </div>
+        </div>
+    </div>
 </main>
